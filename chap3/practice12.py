41b6d349c838a55f6c537170f4a3e1d2db5fdc31
fsggduhu <3145117638@qq.com>
2026-02-12 09:14:15 +0800

    比较，逻辑运算符

---
+++ chap3/practice12.py
@@ -0,0 +1,32 @@
+#比较运算符
+print('98大于90吗?',98>90)
+print('98小于90吗?',98<90)
+print('98等于90吗?',98==90)
+print('98不等于90吗?',98!=90)
+print('98大于等于98吗?',98>=98)
+print('98小于等于98吗?',98<=98)
+
+#逻辑运算符
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+print(not False)
+print(not True)
+#逻辑与实例
+print(8>7 and 6>5)
+print(8>7 and 6<5)
+print(8<7 and 6<5)
+print(8<7 and 6>5)
+#逻辑或实例
+print(8>7 or 6>5)
+print(8>7 or 6<5)
+print(8<7 or 6<5)
+print(8<7 or 6>5)
+#逻辑非实例
+print(not(8>7))
+print(not(8<7))
