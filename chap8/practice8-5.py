52429c4ef68191c233b55b6a4c547d468648b928
fsggduhu <3145117638@qq.com>
2026-02-24 19:39:37 +0800

    内置中的数学函数

---
+++ chap8/practice8-5.py
@@ -0,0 +1,16 @@
+print('绝对值:',abs(100),abs(-100),abs(0))
+print('商和余数:',divmod(13,2))
+print('最大值:',max('hello'))
+print('最大值:',max([10,3,89,23]))
+print('最小值:',min('hello'))
+print('最小值:',min([10,3,89,23]))
+print('求和:',sum([10,20,30]))
+print('x的y次幂:',pow(10,3))
+
+
+#四舍五入
+print(round(3.1415926))
+print(round(3.9415926))
+print(round(3.1415926,2))
+print(round(314.15926,-1))#-1表示个位进行四舍五入
+print(round(314.15926,-2))#-2表示十位进行四舍五入
