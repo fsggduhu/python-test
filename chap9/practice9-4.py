d6000ac5276eaddc67737932f55319c699a87f1e
fsggduhu <3145117638@qq.com>
2026-02-25 21:58:19 +0800

    继承

---
+++ chap9/practice9-4.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self,name,gender):
+        self.name=name
+        self.gender=gender
+
+    def show(self):
+        print(f'我叫{self.name},我的性别是{self.gender}')
+
+#定义两个类
+class Student(Person):
+    #编写初始化方法
+    def __init__(self,name,gender,stuno):
+        super().__init__(name,gender)
+        self.stuno=stuno
+
+class Docter(Person):
+    def __init__(self,name,gender,department):
+        super().__init__(name,gender)
+        self.department=department
+
+#创建第一个子类对象
+stu=Student('陈梅梅','女','1001')
+stu.show()
+
+docter=Docter('张一一','男','外科')
+docter.show()
